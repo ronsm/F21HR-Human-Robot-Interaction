22fede01c455de2101b0c6a33dcfafa21f591ec2
Ronnie Smith <ronniesmith@outlook.com>
2018-11-16 20:32:41 +0000

    Added step 6

--- modules/control.py
+++ modules/control.py
@@ -3,6 +3,7 @@ from search import Search
 from interact import Interact
 from comms import Comms
 import cozmo
+import numpy as np
 import asyncio
 import sys
 from cozmo.util import degrees, distance_mm, speed_mmps
@@ -15,8 +16,10 @@ async def cozmo_program(sdk_conn1, sdk_conn2):
     # We will check whether each step is successful
     res = False
 
+    # currentPos1 = [0, 3, 180] # Initialised at Step 1
+    # currentPos2 = [2, 6, 180] # Initialised at Step 2
+
     # Step 1
-    # currentPos1 = [0, 3, 180]
     # res, currentPos1 = await step_1(robot1, currentPos1) # currentPos = [3, 2, 90]
     # destPos = currentPos1
     # if res == False:
@@ -30,41 +33,44 @@ async def cozmo_program(sdk_conn1, sdk_conn2):
     # print(currentPos1)
 
     # Step 3]
-    destPos = [6, 4, 90]     # USE ONLY IF PREVIOUS STEP IS DISABLED
-    currentPos1 = [6, 4, 90] # USE ONLY IF PREVIOUS STEP IS DISABLED
-    res, destPos = await step_3(robot1, robot2, destPos)
-    if res == False:
-        print('[ERROR][STEP 3] Robot syncronisation process failed.')
+    # destPos = [6, 4, 90]     # USE ONLY IF PREVIOUS STEP IS DISABLED
+    # currentPos1 = [6, 4, 90] # USE ONLY IF PREVIOUS STEP IS DISABLED
+    # res, destPos = await step_3(robot1, robot2, destPos)
+    # if res == False:
+    #     print('[ERROR][STEP 3] Robot syncronisation process failed.')
 
-    # Step 4
-    currentPos1 = [4, 6, 0] # USE ONLY IF PREVIOUS STEP IS DISABLED
-    currentPos2 = [2, 6, 180]
-    res, currentPos1, currentPos2 = await step_4(robot1, robot2, currentPos1, currentPos2, destPos)
-    if res == False:
-        print('[ERROR][STEP 4] Unable to move Robot 1 to observation position.')
-    print(currentPos1, currentPos2)
+    # # Step 4
+    # currentPos1 = [4, 6, 0] # USE ONLY IF PREVIOUS STEP IS DISABLED
+    # res, currentPos1, currentPos2 = await step_4(robot1, robot2, currentPos1, currentPos2, destPos)
+    # if res == False:
+    #     print('[ERROR][STEP 4] Unable to move Robot 1 to observation position.')
+    # print(currentPos1, currentPos2)
 
     # Step 5
+    # await step_5(robot1)
 
     # Step 6
+    currentPos1 = [8, 4, 0]  # USE ONLY IF PREVIOUS STEP IS DISABLED
+    currentPos2 = [6, 4, 90] # USE ONLY IF PREVIOUS STEP IS DISABLED
+    res, currentPos1, currentPos2 = await step_6(robot1, robot2, currentPos1, currentPos2)
 
     # Step 7
-    # step_7(robot)
+    # await step_7(robot1)
     
 
 # ROBOT 1 finds the cube
-async def step_1(robot, currentPos):
-    s = Search(robot, currentPos)
-    found, currentPos = await s.search()
+async def step_1(robot1, currentPos1):
+    s = Search(robot1, currentPos1)
+    found, currentPos1 = await s.search()
     
     if found == True:
-        return True, currentPos
+        return True, currentPos1
     else:
-        return False, currentPos
+        return False, currentPos1
 
 # ROBOT 1 returns to face ROBOT 2
-async def step_2(robot, currentPos):
-    a = AStar(robot, currentPos)
+async def step_2(robot1, currentPos1):
+    a = AStar(robot1, currentPos1)
 
     print('Navigating from:', (6, 4), 'to', (4, 6))
     print('')
@@ -173,21 +179,69 @@ async def step_4(robot1, robot2, currentPos1, currentPos2, destPos):
     return True, currentPos1, currentPos2
 
 # ROBOT 2 picks up the cube
-def step_5(robot, currentPos):
-    pass
+async def step_5(robot2):
+    interact = Interact(robot2)
+
+    initialPose = [robot2.pose.position.x, robot2.pose.position.y]
+    print(initialPose)
+    
+    res = await interact.pickupCube()
+
+    finalPose = [robot2.pose.position.x, robot2.pose.position.y]
+    print(finalPose)
+
+    b = finalPose[0] - initialPose[0]
+    a = finalPose[1] - initialPose[1]
+    hyp = np.sqrt(b**2 - a**2)
+
+    await asyncio.sleep(1)
+
+    await robot2.drive_straight(distance_mm(-hyp), speed_mmps(50)).wait_for_completed()
+
+    print(hyp)
 
 # ROBOT 2 brings the cube to place on map where person can be seen
 # ROBOT 1 follows ROBOT 2 after a pause
-def step_6(robot, currentPos):
-    pass
+async def step_6(robot1, robot2, currentPos1, currentPos2):
+    # ROBOT 2
+    a = AStar(robot2, currentPos2)
+
+    print('Navigating from:', (6, 4), 'to', (4, 0))
+    print('')
+
+    path = a.initLegoWorld((6, 4), (4, 0))
+    for i in range(len(path)):
+        await a.move(path[i])
+
+    await a.face("south")
+    currentPos2 = a.getPos()
+
+
+    # ROBOT 1
+    a = AStar(robot1, currentPos1)
+
+    print('Navigating from:', (8, 4), 'to', (4, 2))
+    print('')
+    
+    path = a.initLegoWorld((8, 4), (4, 2))
+    for i in range(len(path)):
+        await a.move(path[i])
+
+    currentPos1 = a.getPos()
+
+    await robot1.drive_straight(distance_mm(150), speed_mmps(50)).wait_for_completed()
+    await robot1.turn_in_place(degrees(50)).wait_for_completed()
+
+    return True, currentPos1, currentPos2
 
 # ROBOT 2 puts down the cube
 # ROBOT 2 then searches for person
-def step_7(robot):
-    interact = Interact(robot)
-    interact.detectPerson()
+async def step_7(robot1):
+    interact = Interact(robot1)
+    res = await interact.detectPerson()
 
-#cozmo.run_program(cozmo_program, use_viewer=True)
+    if res == True:
+        print('Found a face')
 
 if __name__ == '__main__':
     cozmo.setup_basic_logging()
@@ -198,8 +252,6 @@ if __name__ == '__main__':
         conn2 = cozmo.connect_on_loop(loop)
     except cozmo.ConnectionError as e:
         sys.exit("A connection error occurred: %s" % e)
-
-    #cozmo.run_program(cozmo_program(conn1, conn2), use_viewer=True)
 
     # Run a coroutine controlling both connections
     loop.run_until_complete(cozmo_program(conn1, conn2))
--- modules/interact.py
+++ modules/interact.py
@@ -10,18 +10,23 @@ class Interact(object):
 
         self.robot = robot
 
-    def detectPerson(self):
-        self.robot.move_lift(-3)
-        self.robot.set_head_angle(cozmo.robot.MAX_HEAD_ANGLE).wait_for_completed()
+    async def detectPerson(self):
+        res = False
 
-        face = None
-        firstDetect = True
+        #await self.robot.turn_in_place(degrees(-15)).wait_for_completed()
+
+        self.robot.move_lift(-3)
+        for i in range(10, 50, 10):
+            await self.robot.set_head_angle(degrees(i)).wait_for_completed()
+
+            face = None
+            firstDetect = True
         
-        while True:
             if face and face.is_visible and firstDetect == True:
                 firstDetect = False
                 self.robot.set_all_backpack_lights(cozmo.lights.blue_light)
-                self.robot.say_text("Hello!").wait_for_completed()
+                await self.robot.say_text("Hello There!").wait_for_completed()
+                res = True
             if face and face.is_visible and firstDetect == False:
                 self.robot.set_all_backpack_lights(cozmo.lights.blue_light)
                 time.sleep(3)
@@ -30,11 +35,35 @@ class Interact(object):
                 self.robot.set_backpack_lights_off()
 
                 try:
-                    face = self.robot.world.wait_for_observed_face(timeout=20)
+                    face = await self.robot.world.wait_for_observed_face(timeout=5)
+                    if(face):
+                        self.robot.set_all_backpack_lights(cozmo.lights.blue_light)
+                        await self.robot.say_text("Hello There!").wait_for_completed()
+                        await asyncio.sleep(2)
+                        self.robot.set_all_backpack_lights(cozmo.lights.off_light)
                 except asyncio.TimeoutError:
                     print("Didn't find a face.")
-                    return
 
-            time.sleep(.1)
+            if res == True:
+                break
 
-        self.robot.say_text("DONE")
+            if i == 10:
+                await self.robot.say_text("Hmm...").wait_for_completed()
+            elif i == 20:
+                await self.robot.say_text("Where are you?").wait_for_completed()
+
+        return res
+
+    async def pickupCube(self):
+        #lookaround = self.robot.start_behavior(cozmo.behavior.BehaviorTypes.LookAroundInPlace)
+        cubes = await self.robot.world.wait_until_observe_num_objects(num=1, object_type=cozmo.objects.LightCube, timeout=60)
+        #lookaround.stop()
+
+        max_dst, targ = 0, None
+        for cube in cubes:
+            translation = self.robot.pose - cube.pose
+            dst = translation.position.x ** 2 + translation.position.y ** 2
+            if dst > max_dst:
+                max_dst, targ = dst, cube
+
+            await self.robot.pickup_object(targ, num_retries=3).wait_for_completed()
